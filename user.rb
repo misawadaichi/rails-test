d5f9c419d2d9de0ad64faa0e67a7b9d41447a986
moyasi <moyasi06@gmail.com>
2021-07-18 17:28:40 +0900

    1-4-2

--- user.rb
+++ user.rb
@@ -1,8 +1,28 @@
 class User
   attr_accessor :name, :address, :email
+  def initialize(name, address, email)
+    @name = name
+    @adress = address
+    @email = email
+  end
 
   def profile
     "#{name} (#{address})"
   end
 
+end
+
+class Person
+  def initialize(money)
+    @money = money
+  end
+
+  def billionaire?
+    money >= 100000000
+  end
+
+  private
+  def money
+    @money
+  end
 end
